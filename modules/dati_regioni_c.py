3019a50b49955229d9a3a25b63c15e29679b8ae0
ScelzoF <112801219+ScelzoF@users.noreply.github.com>
2025-03-28 12:12:24 +0100

    Add files via upload

--- modules/dati_regioni_c.py
+++ modules/dati_regioni_c.py
@@ -1,3 +1,9 @@
 dati_regioni_c = {
-"Valle d'Aosta": {'criticita': 'Contenuto in fase di aggiornamento.', 'punti_raccolta': 'Punti di raccolta in fase di integrazione.', 'link_utili': {"Protezione Civile Valle d'Aosta": 'https://www.protezionecivile.gov.it'}
+    "Valle d'Aosta": {
+        'criticita': 'Contenuto in fase di aggiornamento.',
+        'punti_raccolta': 'Punti di raccolta in fase di integrazione.',
+        'link_utili': {
+            "Protezione Civile Valle d'Aosta": 'https://www.protezionecivile.gov.it'
+        }
+    }
 }
