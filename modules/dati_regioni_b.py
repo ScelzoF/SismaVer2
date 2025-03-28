9d95e5a137c5f97121684afc859a49fe423935e4
ScelzoF <112801219+ScelzoF@users.noreply.github.com>
2025-03-28 11:04:50 +0100

    Add files via upload

--- modules/dati_regioni_b.py
+++ modules/dati_regioni_b.py
@@ -24,3 +24,46 @@ Provincia di Chieti
             "descrizione": "In Abruzzo si verificano frequentemente alluvioni e frane, in particolare nelle aree montane appenniniche.",
             "link": "https://www.protezionecivile.gov.it/it/rischi/rischio-idrogeologico/"
         }
+    },"rischio_idrogeologico": {
+            "descrizione": "In Abruzzo si verificano frequentemente alluvioni e frane, in particolare nelle aree montane appenniniche.",
+            "link": "https://www.protezionecivile.gov.it/it/rischi/rischio-idrogeologico/"
+        }
+    },"rischio_idrogeologico": {
+            "descrizione": "Informazioni in fase di completamento.",
+            "link": "https://www.protezionecivile.gov.it/it/rischi/rischio-idrogeologico/"
+        }
+    },
+    "Campania": {
+        "criticita": "Contenuto in fase di aggiornamento.",
+        "punti_raccolta": "Punti di raccolta in fase di integrazione.",
+        "link_utili": {
+            "Protezione Civile Campania": "https://www.protezionecivile.gov.it"
+        },
+        "rischio_idrogeologico": {
+            "descrizione": "Informazioni in fase di completamento.",
+            "link": "https://www.protezionecivile.gov.it/it/rischi/rischio-idrogeologico/"
+        }
+    },
+    "Emilia Romagna": {
+        "criticita": "Contenuto in fase di aggiornamento.",
+        "punti_raccolta": "Punti di raccolta in fase di integrazione.",
+        "link_utili": {
+            "Protezione Civile Emilia Romagna": "https://www.protezionecivile.gov.it"
+        },
+        "rischio_idrogeologico": {
+            "descrizione": "Informazioni in fase di completamento.",
+            "link": "https://www.protezionecivile.gov.it/it/rischi/rischio-idrogeologico/"
+        }
+    },
+    "Friuli Venezia Giulia": {
+        "criticita": "Contenuto in fase di aggiornamento.",
+        "punti_raccolta": "Punti di raccolta in fase di integrazione.",
+        "link_utili": {
+            "Protezione Civile Friuli Venezia Giulia": "https://www.protezionecivile.gov.it"
+        },
+        "rischio_idrogeologico": {
+            "descrizione": "Informazioni in fase di completamento.",
+            "link": "https://www.protezionecivile.gov.it/it/rischi/rischio-idrogeologico/"
+        }
+    },
+}
